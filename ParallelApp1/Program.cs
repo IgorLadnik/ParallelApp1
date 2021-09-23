55449df4cb9dc9d4e3009ca952e59a30f78c5d08
IgorLadnik <ladnik@hotmail.com>
2021-09-23 08:38:26 +0300

    class Processor

--- ParallelApp1/Program.cs
+++ ParallelApp1/Program.cs
@@ -1,9 +1,9 @@
 ﻿// This app (.NET5) processes enqueued messages in parallel with alloeed limited parallel tasks.
 // Tasks limit is INIT_GRANTED_NUM.
-// All messages to be processed are enqueued in advance for simplicity.
 
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,29 +14,29 @@ namespace ParallelApp1
     {
         const int INIT_GRANTED_NUM = 2;
 
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            ConcurrentQueue<Message> cq = new();
-
-            InitEnqueueMessages(cq);
-
-            await cq.ProcessAsync(msg =>
+            Processor processor = new(msg =>
             {
                 //...
                 Thread.Sleep(2000);
                 Console.WriteLine($"    MessageId = {msg.Id}, CurrentThread: {Thread.CurrentThread.ManagedThreadId}");
-            },
-            INIT_GRANTED_NUM);
+            });
+            processor.EnqueueMessages(CreateMessages(0, 10).ToArray());
+
+            processor.Process(INIT_GRANTED_NUM);
+
+            processor.EnqueueMessages(CreateMessages(10, 20).ToArray());
+            processor.EnqueueMessages(new Message { Id = 21 });
 
             Console.WriteLine("Press any key to quit...");
             Console.ReadKey();
         }
 
-        static void InitEnqueueMessages(ConcurrentQueue<Message> cq)
+        static IEnumerable<Message> CreateMessages(int from, int to) 
         {
-            const int n = 11;
-            for (var i = 0; i < n; i++)
-                cq.Enqueue(new() { Id = i + 1 });
+            for (var i = from; i < to; i++)
+                yield return new() { Id = i + 1 };
         }
     }
 
@@ -45,27 +45,40 @@ namespace ParallelApp1
         public int Id { init; get; }
     }
 
-    static class ProcessEx 
+    class Processor 
     {
-        public static async Task ProcessAsync(this ConcurrentQueue<Message> cq, Action<Message> processMessage, int grantedParallel)
+        private ConcurrentQueue<Message> _cq = new();
+        private Action<Message> _processMessage;
+
+        public Processor(Action<Message> processMessage) =>
+            _processMessage = processMessage;
+
+        public void EnqueueMessages(params Message[] messages)
         {
-            using SemaphoreSlim sm = new(grantedParallel);
-            ConcurrentBag<Task> tasks = new();
-            while (cq.Any())
-            {
-                await sm.WaitAsync();
-                tasks.Add(Task.Run(() =>
-                {
-                    Console.WriteLine($"Number of spare threads: {sm.CurrentCount}");
-
-                    if (cq.TryDequeue(out Message msg))
-                        processMessage(msg);
-
-                    sm.Release(1);
-                }));
-            }
-
-            Task.WaitAll(tasks.ToArray());
+            foreach (var msg in messages)
+                _cq.Enqueue(msg);
         }
+
+        public void Process(int grantedParallel) =>
+            Task.Run(async () =>
+            {
+                using SemaphoreSlim sm = new(grantedParallel);
+                ConcurrentBag<Task> tasks = new();
+                while (_cq.Any())
+                {
+                    await sm.WaitAsync();
+                    tasks.Add(Task.Run(() =>
+                    {
+                        Console.WriteLine($"Number of spare threads: {sm.CurrentCount}");
+
+                        if (_processMessage != null && _cq.TryDequeue(out Message msg))
+                            _processMessage(msg);
+
+                        sm.Release(1);
+                    }));
+                }
+
+                Task.WaitAll(tasks.ToArray());
+            });
     }
 }
